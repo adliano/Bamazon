1a08593ca12e5f32f047a56eb8c53cfad9618932
Adliano <adliano@me.com>
2019-05-12 01:47:29 -0700

    init index.js and start the inquirer

---
+++ index.js
@@ -0,0 +1,15 @@
+// http://www.mysqltutorial.org/
+// https://www.npmjs.com/package/inquirer#examples
+
+let inquirer = require('inquirer');
+
+inquirer.prompt([
+    {
+        type: 'list',
+        message : 'What do you like to do?',
+        choices: ['option a','option b'],
+        name: 'todo'
+    }
+]).then((answer) =>{
+    console.log(answer);
+});
