297714232f310abdb4874b5340d6e1fad5f0619c
Adliano <adliano@me.com>
2019-05-12 07:49:52 -0700

    created the product_db schemas

---
+++ bamazonSchemas.sql
@@ -0,0 +1,30 @@
+
+CREATE DATABASE IF NOT EXISTS bamazon_db;
+
+USE bamazon_db;
+
+-- sku (unique id for each product)
+-- product_name (Name of product)
+-- department_name
+-- price (cost to customer)
+-- stock_quantity (how much of the product is available in stores)
+
+CREATE TABLE IF NOT EXISTS products_tb(
+    sku INTEGER AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(32) NOT NULL,
+    department_name VARCHAR(32) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INTEGER DEFAULT 0
+    );
+
+/*
++-----------------+---------------+------+-----+---------+----------------+
+| Field           | Type          | Null | Key | Default | Extra          |
++-----------------+---------------+------+-----+---------+----------------+
+| sku             | int(11)       | NO   | PRI | NULL    | auto_increment |
+| product_name    | varchar(32)   | NO   |     | NULL    |                |
+| department_name | varchar(32)   | NO   |     | NULL    |                |
+| price           | decimal(10,2) | NO   |     | NULL    |                |
+| stock_quantity  | int(11)       | YES  |     | 0       |                |
++-----------------+---------------+------+-----+---------+----------------+
+*/
