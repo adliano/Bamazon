a4740108481bbd6b9e480ed9ba49f73425c206a6
Adliano <adliano@me.com>
2019-05-12 09:01:16 -0700

    changed schemas to void duplicated records

--- bamazonSchemas.sql
+++ bamazonSchemas.sql
@@ -1,4 +1,6 @@
 
+DROP DATABASE bamazon_db;
+
 CREATE DATABASE IF NOT EXISTS bamazon_db;
 
 USE bamazon_db;
@@ -10,11 +12,13 @@ USE bamazon_db;
 -- stock_quantity (how much of the product is available in stores)
 
 CREATE TABLE IF NOT EXISTS products_tb(
-    sku INTEGER AUTO_INCREMENT PRIMARY KEY,
+    sku INTEGER AUTO_INCREMENT,
     product_name VARCHAR(32) NOT NULL,
     department_name VARCHAR(32) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
-    stock_quantity INTEGER DEFAULT 0
+    stock_quantity INTEGER DEFAULT 0,
+    PRIMARY KEY(sku),
+    UNIQUE(product_name)
     );
 
 /*
@@ -22,9 +26,11 @@ CREATE TABLE IF NOT EXISTS products_tb(
 | Field           | Type          | Null | Key | Default | Extra          |
 +-----------------+---------------+------+-----+---------+----------------+
 | sku             | int(11)       | NO   | PRI | NULL    | auto_increment |
-| product_name    | varchar(32)   | NO   |     | NULL    |                |
+| product_name    | varchar(32)   | NO   | PRI | NULL    |                |
 | department_name | varchar(32)   | NO   |     | NULL    |                |
 | price           | decimal(10,2) | NO   |     | NULL    |                |
 | stock_quantity  | int(11)       | YES  |     | 0       |                |
 +-----------------+---------------+------+-----+---------+----------------+
 */
+
+DESCRIBE products_tb;
--- seed.sql
+++ seed.sql
@@ -1,2 +1,4 @@
 INSERT INTO products_tb(product_name,department_name,price,stock_quantity)
 VALUES('macBook pro', 'computers', 1250, 3);
+
+-- http://www.mysqltutorial.org/mysql-delete-duplicate-rows/
