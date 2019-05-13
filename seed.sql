509a3457257f47a3fad33ca7439624aa784a1af4
Adliano <adliano@me.com>
2019-05-12 21:19:53 -0700

    removed node_modules from git, create the .gitignore

--- seed.sql
+++ seed.sql
@@ -1,4 +1,14 @@
-INSERT INTO products_tb(product_name,department_name,price,stock_quantity)
-VALUES('macBook pro', 'computers', 1250, 3);
-
+INSERT INTO
+  products_tb(
+    product_name,
+    department_name,
+    price,
+    stock_quantity
+  )
+VALUES(
+    'macBook pro',
+    'computers',
+    1250,
+    3
+  );
 -- http://www.mysqltutorial.org/mysql-delete-duplicate-rows/
