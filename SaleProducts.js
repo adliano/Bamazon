8ad6b34652ce0261fc9999c2f4ded917288f0aea
Adliano Alves <adliano@me.com>
2019-05-15 20:48:56 -0700

    SalesProduct.js function to return product info its working now

--- SaleProducts.js
+++ SaleProducts.js
@@ -1,69 +1,70 @@
-const mysql = require('mysql');
-const mysqlconfig = require('./MysqlConfig');
+const mysql = require("mysql");
+const mysqlconfig = require("./MysqlConfig");
 const PRODUCT_TABLE = mysqlconfig.PRODUCT_TABLE;
 
 const connector = mysql.createConnection(mysqlconfig.mysqlconfig);
 
+class SaleProducts {
+  constructor(pruductDataObj) {
+    this.sku = pruductDataObj.sku;
+    this.stock_quantity = pruductDataObj.stock_quantity;
+  }
+  /**
+   * Once the customer has placed the order, your application
+   * should check if your store has enough of the product to
+   * meet the customer's request
+   */
+  getProductInfo() {
+    return new Promise((resolve, reject) => {
+      // Connect to server
+      connector.connect(err => {
+        // Check for error on connection
+        if (err) reject(err); //throw err;
+        // query through database.table
+        connector.query(
+          // Query to display all available products
+          `SELECT * FROM ${PRODUCT_TABLE} WHERE sku=${this.sku}`,
+          // Callback used to handle the return from query
+          (err, res) => {
+            // Check for error on querying the database.table
+            if (err) reject(err); //throw err;
+            // Display results using the npm console.table
+            //console.log(`Total Available: ${res[0].stock_quantity}`);
+            //let stock = res[0].stock_quantity;
+            // close connection to mysql server
+            connector.end();
+            // Return total available product
+            //resolve(stock);
+            resolve(res[0]);
+          }
+        );
+      });
+    });
+  }
+  /**
+   *
+   */
+  //   getProductName() {
+  //     // Connect to server
+  //     connector.connect(err => {
+  //       // Check for error on connection
+  //       if (err) throw err;
+  //       // query through database.table
+  //       connector.query(
+  //         // Query to display all available products
+  //         `SELECT product_name FROM ${PRODUCT_TABLE} WHERE sku=${this.sku}`,
+  //         // Callback used to handle the return from query
+  //         (err, res) => {
+  //           // Check for error on querying the database.table
+  //           if (err) throw err;
+  //           // close connection to mysql server
+  //           connector.end();
 
-class SaleProducts{
-    constructor(pruductDataObj){
-        this.sku = pruductDataObj.sku;
-        this.stock_quantity = pruductDataObj.stock_quantity;
-    }
-    /**
-     * Once the customer has placed the order, your application 
-     * should check if your store has enough of the product to 
-     * meet the customer's request
-     */
-    getStock(){
-        // Connect to server
-        connector.connect((err) => {
-            // Check for error on connection
-            if (err) throw err;
-            // query through database.table
-            connector.query(
-                // Query to display all available products
-                `SELECT * FROM ${PRODUCT_TABLE} WHERE sku=${this.sku}`,
-                // Callback used to handle the return from query
-                (err, res) => {
-                    // Check for error on querying the database.table
-                    if (err) throw err;
-                    // Display results using the npm console.table
-                    //console.log(`Total Available: ${res[0].stock_quantity}`);
-                    let stock = res[0].stock_quantity;
-                    // close connection to mysql server
-                    connector.end();
-                    // Return total available product
-                    console.log(stock);
-                    
-                    return stock;
-                });
-        });
-    }
-    /**
-     * 
-     */
-    getProductName(){
-        // Connect to server
-        connector.connect((err) => {
-            // Check for error on connection
-            if (err) throw err;
-            // query through database.table
-            connector.query(
-                // Query to display all available products
-                `SELECT product_name FROM ${PRODUCT_TABLE} WHERE sku=${this.sku}`,
-                // Callback used to handle the return from query
-                (err, res) => {
-                    // Check for error on querying the database.table
-                    if (err) throw err;
-                    // close connection to mysql server
-                    connector.end();
-
-                    return res;
-                });
-        });
-
-    }
+  //           return res;
+  //         }
+  //       );
+  //     });
+  //   }
 }
 
 module.exports = SaleProducts;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,36 +1,48 @@
 // http://www.mysqltutorial.org/
 // https://www.npmjs.com/package/inquirer#examples
 /**
- * Running this application will first display all of 
+ * Running this application will first display all of
  * the items available for sale. Include the skus, names,
  * and prices of products for sale.
  */
 
-let ListProducts = require('./ListProducts');
-let SaleProducts = require('./SaleProducts');
-let inquirer = require('inquirer');
+let ListProducts = require("./ListProducts");
+let SaleProducts = require("./SaleProducts");
+let inquirer = require("inquirer");
 
 // Function (Callback) used to display questions about sale products
 function sales() {
-    // The first should ask them the SKU of the product they would like to buy
-    inquirer.prompt([{
-            // The first should ask them the SKU of the product they would like to buy.
-            type: 'input',
-            message: 'Enter SKU of the product you like to buy?',
-            name: 'sku'
-        },
-        // The second message should ask how many units of the product they would like to buy.
-        {
-            type: 'input',
-            message: 'How many units of the product would you like to buy?',
-            name: 'stock_quantity'
-        }
-    ]).then((answer) => {
-        // Once the customer has placed the order, your application should check if 
-        // your store has enough of the product to meet the customer's request.
-        let sales = new SaleProducts(answer);
+  // The first should ask them the SKU of the product they would like to buy
+  inquirer
+    .prompt([
+      {
+        // The first should ask them the SKU of the product they would like to buy.
+        type: "input",
+        message: "Enter SKU of the product you like to buy?",
+        name: "sku"
+      },
+      // The second message should ask how many units of the product they would like to buy.
+      {
+        type: "input",
+        message: "How many units of the product would you like to buy?",
+        name: "stock_quantity"
+      }
+    ])
+    .then(answer => {
+      // Once the customer has placed the order, your application should check if
+      // your store has enough of the product to meet the customer's request.
+      let sales = new SaleProducts(answer);
 
-        console.log(sales.getStock());        
+      sales
+        .getProductInfo()
+        .then(x => {
+          console.log("-".repeat(40));
+          console.log(x);
+          console.log("-".repeat(40));
+        })
+        .catch(err => {
+          console.log(err);
+        });
     });
 }
 
