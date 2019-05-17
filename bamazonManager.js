568764cedcd948e063db7c3ef86b49acff2c3267
Adliano Alves <adliano@me.com>
2019-05-17 00:54:21 -0700

    started bamazon manager

---
+++ bamazonManager.js
@@ -0,0 +1,49 @@
+// * List a set of menu options:
+// * View Products for Sale
+// * View Low Inventory
+// * Add to Inventory
+// * Add New Product
+// * If a manager selects `View Products for Sale`,
+//   the app should list every available item: the item SKUs, names, prices, and quantities.
+// * If a manager selects `View Low Inventory`,
+//   then it should list all items with an inventory count lower than five.
+// * If a manager selects `Add to Inventory`, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+// * If a manager selects `Add New Product`, it should allow the manager to add a completely new product to the store.
+
+let inquirer = require("inquirer");
+let ListProducts = require("./ListProducts");
+
+inquirer
+  .prompt([
+    {
+      type: "list",
+      // List a set of menu options:
+      choices: [
+        "View Products for Sale",
+        "View Low Inventory",
+        "Add to Inventory",
+        "Add New Product"
+      ],
+      message: "Options:",
+      name: "option"
+    }
+  ])
+  .then(opt => {
+    switch (opt.option) {
+      case "View Products for Sale":
+        new ListProducts();
+        break;
+      case "View Low Inventory":
+        console.log("view low inventory");
+        break;
+      case "Add to Inventory":
+        console.log("add invß");
+        break;
+      case "Add New Product":
+        console.log("add product");
+        break;
+      default:
+        console.log("OPS!");
+        break;
+    }
+  });
